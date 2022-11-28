41c69a99d268931951bcf30ec409366baa0d2ed3
miguelmendez418 <113858853+miguelmendez418@users.noreply.github.com>
2022-11-27 18:49:25 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         ' us-west-2_Xkqw6CYXy',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '4abnaaohpv3qab53msen6j3q9j',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
